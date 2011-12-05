02401adf666b616457181ddb8be116348d4ed2de
Chris Higgins <chris@chigs.me>
2011-12-05 16:49:15 -0600

    Changed all user views from erb to haml

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -36,7 +36,8 @@ class UsersController < ApplicationController
 
   # GET /users/1/edit
   def edit
-    @user = User.find(params[:id])
+    #@user = User.find(params[:id])
+    @user = User.find_by_username(params[:id])
   end
 
   # POST /users
